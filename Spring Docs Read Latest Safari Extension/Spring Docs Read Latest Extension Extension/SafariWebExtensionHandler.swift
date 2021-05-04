acfe7030b11e7ed0aeb33ae7da35eb400aa37cc7
gabac <cyril@3ap.ch>
2021-05-04 12:07:56 +0200

    Convert existing extension with safari-web-extension-converter

---
+++ Spring Docs Read Latest Safari Extension/Spring Docs Read Latest Extension Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,26 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Spring Docs Read Latest Extension Extension
+//
+//  Created by Cyril Gabathuler on 04.05.21.
+//
+
+import SafariServices
+import os.log
+
+let SFExtensionMessageKey = "message"
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+	func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+    
+}
