65505ac727d7f967256cfd6e754bdc8a58b47ff1
Ted M. Young <tedyoung@gmail.com>
2022-12-26 13:42:11 -0800

    Changed layout from absolute to relative, since it will be pre-pended before the first element in the body anyway.
    Added a close button to get rid of the header (since I don't always want the latest docs, especially during transition from 2.x to 3.x of Spring Boot).
    
    Updated version to 0.0.3.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,5 +1,27 @@
 const supportedProjects = ['spring-boot', 'spring-framework'];
 
+function addLaterVersionHeader(project, urlLatestVersion) {
+	const status = document.createElement('div');
+	status.classList.add('spring-latest-docs-note');
+	status.id = 'spring-latest-docs-note';
+	status.innerHTML = `
+						<div>This is the documentation for an older version of <strong>${project}</strong>. 
+						<a href="${urlLatestVersion}">
+						   <strong>Read Latest</strong>
+						</a>
+            			</div>
+            			<button id='spring-latest-docs-note-close'>Close</button>`;
+
+	document.body.prepend(status);
+
+	const closeButton = document.getElementById('spring-latest-docs-note-close')
+	closeButton.addEventListener('click', function() {
+				const div = document.getElementById('spring-latest-docs-note')
+				document.body.removeChild(div)
+			}
+	);
+}
+
 chrome.extension.sendMessage({}, function (response) {
     var readyStateCheckInterval = setInterval(function () {
         if (document.readyState === "complete") {
@@ -12,18 +34,9 @@ chrome.extension.sendMessage({}, function (response) {
 				const { groups: { project, version } } = regExp.exec(url);
 
 				if (supportedProjects.includes(project) && version !== 'current') {
-					const currentUrl = url.replace(regExp, `$1${project}$3current/$5`)
+					const urlLatestVersion = url.replace(regExp, `$1${project}$3current/$5`)
 
-					const status = document.createElement("div")
-					status.classList.add("spring-latest-docs-note")
-					status.innerHTML = `
-						This is the documentation for an older version of <strong>${project}</strong>. 
-						<a href="${currentUrl}">
-						   <strong>Read Latest</strong>
-						</a>`;
-
-					document.body.prepend(status);
-					document.body.style.paddingTop += 20;
+					addLaterVersionHeader(project, urlLatestVersion);
 				}
 			}
         }
